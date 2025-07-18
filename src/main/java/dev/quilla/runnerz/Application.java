01cd438c0a753bbd20bdf13035cecdcbd9758f0a
quill-a <gabesander@gmail.com>
2025-07-18 17:16:44 -0500

    Implement an HTTP client

--- src/main/java/dev/quilla/runnerz/Application.java
+++ src/main/java/dev/quilla/runnerz/Application.java
@@ -1,6 +1,7 @@
 package dev.quilla.runnerz;
 
 import dev.quilla.runnerz.user.User;
+import dev.quilla.runnerz.user.UserHttpClient;
 import dev.quilla.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -21,7 +25,14 @@ public class Application {
 	}
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 			List<User> users = client.findAll();
 			System.out.println(users);
